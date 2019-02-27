a7da086b3523c21a7bdddbd3517cc3a997486788
Zahid Iqbal Tushar <zi.tushar@live.com>
2019-02-27 12:44:25 +0600

    WebApi and Data class library added

---
+++ cs_aspcore_webapi/Data/models/AwbuildVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.models
+{
+    public partial class AwbuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; }
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
